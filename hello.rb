c42dfd3cfbc5ae6ae336969b2e48a0aeefeea13f
Sam Backus <sbackus@kayak.com>
2015-06-11 21:27:02 -0400

    use twilio to send text

--- hello.rb
+++ hello.rb
@@ -1,5 +1,20 @@
 require 'sinatra'
+require 'twilio-ruby' 
 
 get '/' do
 	erb :message_form
+end
+
+post '/send_message' do
+	account_sid = ENV['TWILIO_ACCOUNT_SID']  
+	auth_token = ENV['TWILIO_AUTH_TOKEN'] 
+	 
+	@client = Twilio::REST::Client.new account_sid, auth_token 
+	 
+	@client.account.messages.create({
+		:from => '+18454434561', 
+		:to => params[:phone_number],
+		:body => params[:message]  
+	})
+	return "sending '#{params[:message]}' to #{params[:phone_number]}."
 end
